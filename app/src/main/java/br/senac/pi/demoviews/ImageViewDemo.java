367947ab2656bcfdd60440dbb6b742c139d12c0f
Cleyson Lima <cleysonph@gmail.com>
2015-10-28 16:18:10 -0300

    Primeiro commit do projeto

---
+++ app/src/main/java/br/senac/pi/demoviews/ImageViewDemo.java
@@ -0,0 +1,13 @@
+package br.senac.pi.demoviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageViewDemo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_view_demo);
+    }
+}
---
+++ app/src/main/java/br/senac/pi/demoviews/MainActivity.java
@@ -0,0 +1,94 @@
+package br.senac.pi.demoviews;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnTextViewDemo).setOnClickListener(abrirTextViewDemo());
+        findViewById(R.id.btnEditTextDemo).setOnClickListener(abrirEditTextDemo());
+        findViewById(R.id.btnImageViewDemo).setOnClickListener(abrirImageViewDemo());
+        findViewById(R.id.btnAutoCompleteDemo).setOnClickListener(abrirAutoCompleteDemo());
+        findViewById(R.id.btnImageButtonDemo).setOnClickListener(abrirImageButton());
+        findViewById(R.id.btnCheckbox).setOnClickListener(abrirCheckBoxToggleButtonActivity());
+        findViewById(R.id.btnProgressDiologDemo).setOnClickListener(abrirProgressDialogDemoActivity());
+    }
+
+    private View.OnClickListener abrirTextViewDemo() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, TextViewDemo.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener abrirEditTextDemo() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditTextDemo.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener abrirImageViewDemo() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ImageViewDemo.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener abrirAutoCompleteDemo() {
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, AutoCompleteDemo.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener abrirImageButton(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ImageButton.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener abrirCheckBoxToggleButtonActivity(){
+        return new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, CheckBoxToggleButtonActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
+
+    private View.OnClickListener abrirProgressDialogDemoActivity(){
+        return new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ProgressDialogDemoActivity.class);
+                startActivity(intent);
+            }
+        };
+    }
+}
